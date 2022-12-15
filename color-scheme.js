5d2f9c194dd2fe8038b18872202d37d766daca04
Eric Tsai <hiphop200199@gmail.com>
2022-12-15 09:18:09 +0800

    設定完color-scheme的code

--- color-scheme.js
+++ color-scheme.js
@@ -1,5 +1,5 @@
 //如果server需要資料就用cookie，如果只有使用者要存那就localstorage
-
+//記得使用者當初選的設定
 let darkMode=localStorage.getItem("darkMode");
 const darkModeToggle=document.querySelector("#btn-tgl");
 const enableDarkMode=()=>{
@@ -10,11 +10,19 @@ const disableDarkMode=()=>{
     document.body.classList.remove("darkmode");
     localStorage.setItem("darkMode",null);
 }
+
+if(darkMode==="enabled"){
+    enableDarkMode();
+}
+
 darkModeToggle.addEventListener("click",()=>{
     darkMode=localStorage.getItem("darkMode");
     if(darkMode!=="enabled"){
         enableDarkMode();
+      
     }else{
         disableDarkMode();
+       
+
     }
 });
